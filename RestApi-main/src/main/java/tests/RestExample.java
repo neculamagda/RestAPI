fd0350d532c388e54f5d411f1f5dc48c246adfae
Ale <necale48@gmail.com>
2024-01-12 07:02:33 -0800

    proiect REST API

---
+++ RestApi-main/src/main/java/tests/RestExample.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+
+public class RestExample {
+
+	
+	@Test
+	public void restExampleTest() {
+		
+		
+		
+
+	}
+	
+	
+	
+	
+}
